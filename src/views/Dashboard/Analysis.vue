293fac4c9ee553b3a0cd905b2250bc08705cd14c
liuxinqiong <372513646@qq.com>
2019-11-04 02:35:04 +0800

    add mock

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,9 +1,54 @@
 <template>
-  <div>分析页</div>
+  <div>
+    <Charts :option="chartOption" style="height: 400px;"></Charts>
+  </div>
 </template>
 
 <script>
-export default {};
+import Charts from "../../components/Charts";
+import axios from "axios";
+export default {
+  components: {
+    Charts
+  },
+  data() {
+    return {
+      chartOption: {}
+    };
+  },
+  mounted() {
+    this.getChartData();
+  },
+  methods: {
+    getChartData() {
+      axios
+        .get("/api/dashboard/chart", {
+          params: {
+            ID: 12345
+          }
+        })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: "123"
+            },
+            tooltip: {},
+            xAxis: {
+              data: ["1", "2", "3", "4", "5", "6"]
+            },
+            yAxis: {},
+            series: [
+              {
+                name: "111",
+                type: "bar",
+                data: response.data
+              }
+            ]
+          };
+        });
+    }
+  }
+};
 </script>
 
 <style></style>
